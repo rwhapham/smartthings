16b3c8398a9fbd5dd3f15581ebb297b94fbe6171
Rob Whapham <rob.whapham@harman.com>
2017-02-21 09:56:49 -0600

    Updates

--- BarnAutomation/BarnAutomation.groovy
+++ BarnAutomation/BarnAutomation.groovy
@@ -21,17 +21,20 @@ preferences {
         section("Audio notification on open:")
         {
             input "notifyOnOpenSensors", "capability.contactSensor", title: "Which?", multiple: true, required: false
+            input "notifyOnOpenToneDevice", "capability.tone", title: "Trigger beep?", required: false
         }
 
         section("Audio notification on close:")
         {
             input "notifyOnCloseSensors", "capability.contactSensor", title: "Which?", multiple: true, required: false
+            input "notifyOnCloseToneDevice", "capability.tone", title: "Trigger beep?", required: false
         }
 
 		section("Monitor things left open:")
         {
             input "leftOpenSensors", "capability.contactSensor", title: "Which?", multiple: true, required: false
             input "leftOpenDuration", "number", title: "For how long (mins)?", defaultValue: 15, range: "1..30", required: false
+            input "leftOpenToneDevice", "capability.tone", title: "Trigger beep?", required: false
         }
 
         section("Monitor temperature:")
@@ -39,11 +42,13 @@ preferences {
             input "tempSensors", "capability.temperatureMeasurement", title: "Which?", multiple: true, required: false
             input "tempLow", "number", title: "For temperatures below?", defaultValue: 35, range: "0..100", required: false
             input "tempHigh", "number", title: "For temperatures above?", defaultValue: 85, range: "0..100", required: false
+            input "tempAlarmDevice", "capability.alarm", title: "Trigger alarm?", required: false
         }
         
         section("Doorbell notification:")
         {
             input "buttonDevice", "capability.button", title: "Which?", required: false
+            input "doorbellPausePlayers", "capability.musicPlayer", title: "Pause playback on?", multiple: true, required: false
         }
         
         section("Knock notification:")
@@ -53,15 +58,30 @@ preferences {
             input "knockDoorbellDevice", "capability.switch", title: "Trigger doorbell?", required: false
         }
         
+        section("Monitor smoke/CO2:")
+        {
+        	input "smokeSensors", "capability.smokeDetector", title: "Which?", multiple: true, required: false
+            input "smokeStopPlayers", "capability.musicPlayer", title: "Stop playback on?", multiple: true, required: false
+        }
+        
+        section("Monitor leaks:")
+        {
+        	input "waterSensors", "capability.waterSensor", title: "Which?", multiple: true, required: false
+            input "waterStopPlayers", "capability.musicPlayer", title: "Stop playback on?", multiple: true, required: false
+            input "waterAlarmDevice", "capability.alarm", title: "Trigger alarm?", required: false
+        }
+        
         section("Audio notification player:")
         {
             input "audioDevice", "capability.audioNotification", title: "Which?", required: false
+            input "audioDeviceVolume", "number", title: "Notification volume?", defaultValue: 8, range: "1..10", required: false
         }
     }
     
     page(name: "page2", title: "Open/Close Audio Notifications", nextPage: "page3", install: false, uninstall: true)
     page(name: "page3", title: "Left Open Audio Notifications", nextPage: "page4", install: false, uninstall: true)
-    page(name: "page4", title: "Temperature Warning Audio Notifications", install: true, uninstall: true)
+    page(name: "page4", title: "Temperature Warning Audio Notifications", nextPage: "page5", install: false, uninstall: true)
+    page(name: "page5", title: "Water Leak Audio Notifications", install: true, uninstall: true)
 }
 
 def page2() {
@@ -96,11 +116,17 @@ def page2() {
             else
             {
                 section("No open/close devices selected")
+                {
+                	paragraph "No open/close devices selected"
+                }
             }
         }
         else
         {
-        	section("No audio notification player selected")
+        	section()
+            {
+            	paragraph "No audio notification player selected"
+            }
         }
 	}
 }
@@ -125,12 +151,18 @@ def page3() {
             }
             else
             {
-                section("No left open devices selected")
+                section()
+                {
+					paragraph "No left open devices selected"
+                }
             }
         }
         else
         {
-        	section("No audio notification player selected")
+        	section()
+            {
+            	paragraph "No audio notification player selected"
+            }
         }
 	}
 }
@@ -156,12 +188,54 @@ def page4() {
             }
             else
             {
-                section("No temperature sensors selected")
+                section()
+                {
+                	paragraph "No temperature sensors selected"
+                }
             }
         }
         else
         {
-        	section("No audio notification player selected")
+        	section()
+            {
+            	paragraph "No audio notification player selected"
+            }
+        }
+    }
+}
+
+def page5() {
+	dynamicPage(name: "page5")
+    {
+    	if (audioDevice)
+        {
+            if (waterSensors)
+            {
+                def pos = 0
+
+                waterSensors.each {
+                    section("$it.displayName leak notification:")
+                    {
+                        input "leakAudioTrack" + pos, "text", title: "Audio track?", required: false
+                    }
+
+                    pos++
+                }
+            }
+            else
+            {
+                section()
+                {
+                	paragraph "No water sensors selected"
+                }
+            }
+        }
+        else
+        {
+        	section()
+            {
+            	paragraph "No audio notification player selected"
+            }
         }
     }
 }
@@ -181,6 +255,8 @@ def updated() {
 }
 
 def initialize() {
+	state.triggerDoorbell = false
+    
 	if (notifyOnOpenSensors)
     	subscribe(notifyOnOpenSensors, "contact.open", onOpenHandler)
         
@@ -205,44 +281,99 @@ def initialize() {
 		subscribe(tempSensors, "temperature", temperatureHandler)
     
     if (buttonDevice)
-    	subscribe(buttonDevice, "button", doorbellHandler)
+    	subscribe(buttonDevice, "button.pushed", doorbellHandler)
         
     if (knockSensors)
     	subscribe(knockSensors, "acceleration.active", knockHandler)
+        
+    if (smokeSensors)
+    {
+    	subscribe(smokeSensors, "smoke", smokeHandler)
+        subscribe(smokeSensors, "carbonMonoxide", smokeHandler)
+    }
+    
+    if (waterSensors)
+    	subscribe(waterSensors, "water", waterHandler)
+}
+
+def playNotificationTrack(data)
+{
+	if (audioDevice && data.audioTrack)
+    {
+        log.info "playNotificationTrack - $audioDevice.displayName play track $data.audioTrack"
+
+        audioDevice.playTrackAtVolume(data.audioTrack, audioDeviceVolume ?: 8)
+    }
 }
 
 def onOpenHandler(evt)
 {
+	def tonePlayed = false
+    
 	log.debug "$evt.displayName was opened"
     
-    def pos = notifyOnOpenSensors.findIndexOf { it.id == evt.deviceId }
-    
-    if (audioDevice && (pos >= 0))
+    if (notifyOnOpenToneDevice)
     {
-        def audioTrackName = "onOpenAudioTrack" + pos
-        def audioTrack = this."$audioTrackName"
+    	notifyOnOpenToneDevice.beep()
+        tonePlayed = true
+    }
+    
+    if (audioDevice)
+    {
+        def pos = notifyOnOpenSensors.findIndexOf { it.id == evt.deviceId }
         
-        if (audioTrack)
+        if (pos >= 0)
         {
-            log.info "onOpenHandler - $audioDevice.displayName play track $audioTrack"
+            def audioTrackName = "onOpenAudioTrack" + pos
+            def audioTrack = this."$audioTrackName"
+
+            if (audioTrack)
+            {
+                if (tonePlayed)
+                {
+					runIn(2, playNotificationTrack, [overwrite: false, data: [audioTrack: audioTrack]])
+                }
+                else
+                {
+                	playNotificationTrack([audioTrack: audioTrack])
+                }
+            }
         }
     }
 }
 
 def onClosedHandler(evt)
 {
+	def tonePlayed = false
+    
 	log.debug "$evt.displayName was closed"
     
-    def pos = notifyOnCloseSensors.findIndexOf { it.id == evt.deviceId }
-    
-	if (audioDevice && (pos >= 0))
+    if (notifyOnOpenToneDevice)
     {
-        def audioTrackName = "onCloseAudioTrack" + pos
-        def audioTrack = this."$audioTrackName"
-
-		if (audioTrack)
+    	notifyOnOpenToneDevice.beep()
+        tonePlayed = true
+    }
+    
+    if (audioDevice)
+    {
+        def pos = notifyOnCloseSensors.findIndexOf { it.id == evt.deviceId }
+        
+        if (pos >= 0)
         {
-            log.info "onOpenHandler - $audioDevice.displayName play track $audioTrack"
+            def audioTrackName = "onCloseAudioTrack" + pos
+            def audioTrack = this."$audioTrackName"
+
+            if (audioTrack)
+            {
+                if (tonePlayed)
+                {
+					runIn(2, playNotificationTrack, [overwrite: false, data: [audioTrack: audioTrack]])
+                }
+                else
+                {
+                	playNotificationTrack([audioTrack: audioTrack])
+                }
+            }
         }
     }
 }
@@ -269,9 +400,17 @@ def leftOpenHandler(data)
 
             if (elapsed >= ((leftOpenDuration * 60000) - 1000))
             {
+            	def tonePlayed = false
+                
                 log.info "$device.displayName was left open"
 
                 sendPush ("$device.displayName was left open")
+                
+                if (leftOpenToneDevice)
+                {
+                	leftOpenToneDevice.beep()
+                    tonePlayed = true
+                }
 
 				if (audioDevice)
                 {
@@ -280,7 +419,14 @@ def leftOpenHandler(data)
 
                     if (audioTrack)
                     {
-                        log.info "leftOpenHandler - $audioDevice.displayName play track $audioTrack"
+                        if (tonePlayed)
+                        {
+                            runIn(2, playNotificationTrack, [overwrite: false, data: [audioTrack: audioTrack]])
+                        }
+                        else
+                        {
+                            playNotificationTrack([audioTrack: audioTrack])
+                        }
                     }
                 }
             }
@@ -309,48 +455,93 @@ def temperatureHandler(evt)
         def actualTempHigh = (tempHigh) ? tempHigh : 85
         def lastTemp = (recentTempEvents && (recentTempEvents.size () > 1)) ? recentTempEvents[1].doubleValue : -1
         def prevTemp = (recentTempEvents && (recentTempEvents.size () > 2)) ? recentTempEvents[2].doubleValue : -1
+        def playTone = false
+        def audioTrackName = null
+        def lowAudioTrack = null
+        def highAudioTrack = null
+        def audioDelay = 0
 
         log.debug "last temperatures from $evt.displayName: $lastTemp, $prevTemp"
 
         if ((evt.doubleValue <= actualTempLow) && ((lastTemp == -1) || (lastTemp > actualTempLow)) && ((prevTemp == -1) || (prevTemp > actualTempLow)))
         {
-            sendPush("Low temperature warning from $evt.displayName: $evt.value")
+			sendPush("Low temperature warning from $evt.displayName: $evt.value")
 
-            if (audioDevice)
-            {
-                def audioTrackName = "lowTempAudioTrack" + pos
-                def audioTrack = this."$audioTrackName"
-
-                if (audioTrack)
-                {
-                    log.info "temperatureHandler (low) - $audioDevice.displayName play track $audioTrack"
-                }
-            }
+			playTone = true
+			audioTrackName = "lowTempAudioTrack" + pos
+			lowAudioTrack = this."$audioTrackName"
         }
 
         if ((evt.doubleValue >= actualTempHigh) && ((lastTemp == -1) || (lastTemp < actualTempHigh)) && ((lastTemp == -1) || (lastTemp < actualTempHigh)))
         {
             sendPush("High temperature warning from $evt.displayName: $evt.value")
 
-            if (audioDevice)
+			playTone = true
+			audioTrackName = "highTempAudioTrack" + pos
+			highAudioTrack = this."$audioTrackName"
+        }
+        
+        if (playTone && tempAlarmDevice)
+        {
+        	tempAlarmDevice.siren()
+            audioDelay += 2
+        }
+        
+        if (audioDevice && (lowAudioTrack || highAudioTrack))
+        {
+            if (lowAudioTrack)
             {
-                def audioTrackName = "highTempAudioTrack" + pos
-                def audioTrack = this."$audioTrackName"
-
-                if (audioTrack)
+                if (audioDelay > 0)
                 {
-                    log.info "temperatureHandler (high) - $audioDevice.displayName play track $audioTrack"
+                    runIn(audioDelay, playNotificationTrack, [overwrite: false, data: [audioTrack: lowAudioTrack]])
+                }
+                else
+                {
+                    playNotificationTrack([audioTrack: lowAudioTrack])
+                }
+                
+                audioDelay += 3
+            }
+
+            if (highAudioTrack)
+            {
+                if (audioDelay > 0)
+                {
+                    runIn(audioDelay, playNotificationTrack, [overwrite: false, data: [audioTrack: highAudioTrack]])
+                }
+                else
+                {
+                    playNotificationTrack([audioTrack: highAudioTrack])
                 }
             }
-        }
+		}
 	}
 }
 
 def doorbellHandler(evt)
 {
-	log.info "$evt.displayName was pressed"
+	if (doorbellPausePlayers)
+    {
+    	doorbellPausePlayers.each {
+        	if (it.latestValue("status") == "playing")
+            {
+            	log.info "doorbellHandler - pausing $it.displayName"
+                
+            	it.pause()
+            }
+        }
+    }
     
-    sendPushMessage("$evt.displayName was pressed")
+	if (state.triggerDoorbell == false)
+    {
+        log.info "$evt.displayName was pressed"
+
+        sendPushMessage("$evt.displayName was pressed")
+    }
+    else
+    {
+    	state.triggerDoorbell = false
+    }
 }
 
 def knockHandler(evt)
@@ -383,11 +574,13 @@ def doorKnock(data)
                     {
                         log.info "doorKnock - knocking at $device.displayName"
 
-                        //sendPushMessage("Knocking at $device.displayName")
+                        sendPushMessage("Knocking at $device.displayName")
 
                         if (knockDoorbellDevice)
                         {
-                            //knockDoorbellDevice.on ()
+                        	state.triggerDoorbell = true
+                            
+                            knockDoorbellDevice.on ()
                         }
                     }
                     else
@@ -408,6 +601,79 @@ def doorKnock(data)
         catch (e)
         {
         	log.error ("doorKnock - caught exception", e)
+        }
+    }
+}
+
+def smokeHandler(evt)
+{
+    log.debug "smokeHandler - $evt.displayName reported $evt.value"
+
+	if (evt.value != "clear")
+    {
+    	if (smokeStopPlayers)
+        {
+            smokeStopPlayers.each {
+                if (it.latestValue("status") == "playing")
+                {
+                    log.info "smokeHandler - stopping $it.displayName"
+
+                    it.stop()
+                }
+            }
+        }
+    }
+}
+
+def waterHandler(evt)
+{
+    log.debug "waterHandler - $evt.displayName reported $evt.value"
+    
+	if (evt.value != "dry")
+    {
+        def tonePlayed = false
+
+        sendPush("Leak detected by $evt.displayName")
+        
+		if (waterStopPlayers)
+        {
+            waterStopPlayers.each {
+                if (it.latestValue("status") == "playing")
+                {
+                    log.info "waterHandler - stopping $it.displayName"
+
+                    it.stop()
+                }
+            }
+        }
+        
+        if (waterAlarmDevice)
+        {
+            waterAlarmDevice.siren()
+            tonePlayed = true
+        }
+
+        if (audioDevice)
+        {
+            def pos = waterSensors.findIndexOf { it.id == evt.deviceId }
+
+            if (pos >= 0)
+            {
+                def audioTrackName = "leakAudioTrack" + pos
+                def audioTrack = this."$audioTrackName"
+
+                if (audioTrack)
+                {
+                    if (tonePlayed)
+                    {
+                        runIn(2, playNotificationTrack, [overwrite: false, data: [audioTrack: audioTrack]])
+                    }
+                    else
+                    {
+                        playNotificationTrack([audioTrack: audioTrack])
+                    }
+                }
+            }
         }
     }
 }
